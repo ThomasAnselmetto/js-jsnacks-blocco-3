a96b55cc4c1cc0c49779169f5b8b6a70634f985d
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-01-30 16:04:55 +0100

    snack1 ended

--- Snack1/main1.js
+++ Snack1/main1.js
@@ -1,6 +1,6 @@
 //Ottieni un numero casuale in un range specificato. Chiedi all'utente il valore minimo ed il valore massimo del range in cui generare il numero.
 
-// creare un arrai con un push dato dal Math.random che si trova nel range dell'utente
+// creare un array con un push dato dal Math.random che si trova nel range dell'utente
 
 // richiedi all'utente un valore minimo ed un valore massimo per stabilire il range
 
@@ -16,6 +16,6 @@ numbers.push(valoreMassimo);
 console.log(numbers);
 
 
-
-Math.floor(Math.random() * numbers.length);
+let numeroRandomico = Math.floor(Math.random() * (valoreMassimo - valoreMinimo)) + valoreMinimo;
+console.log(numeroRandomico);
 
