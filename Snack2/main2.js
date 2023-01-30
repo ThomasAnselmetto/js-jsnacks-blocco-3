49681b99d1408467019f0e48991db244cf97d1b5
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-01-30 14:43:24 +0100

    init commit

---
+++ Snack2/main2.js
@@ -0,0 +1,20 @@
+// Snack2
+// Crea un array vuoto e chiedi all'utente un numero da inserire nell'array.Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+// creo un array vuoto
+// creo un prompt o un input per chiedere numeri all'utente
+// creo un cinclo in cui inserisco un numero e lo faccio sommare al precedente,la condizione e' che la somma debba essere inferiore a 50,quando raggiungo 50 esco da ciclo
+
+// let numbers = [];
+
+let userNumberEl = document.getElementById("user-number");
+let buttonEl = document.getElementById("button");
+
+buttonEl.addEventListener(
+    "click",
+    function () {
+        ;
+    }
+)
+
+
