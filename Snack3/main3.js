46173f4d0ddf28d3dadcaa1062087e68000c2145
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-01-31 14:16:26 +0100

    ended snack3

--- Snack3/main3.js
+++ Snack3/main3.js
@@ -1,22 +1,37 @@
 // Snack3
 // Fai inserire un numero, che chiameremo N, all'utente. Genera N array, ognuno formato da 10 numeri casuali da 1 a 100. Ogni volta che ne crei uno, stampalo.
 
+// chiedo N
+let n = parseInt(prompt("Inserisci N"));
 
-let userArrayNumber = parseFloat(prompt("inserisci un numero per stabilire gli arry da creare!!"));
-
-console.log(userArrayNumber);
-
-
-
-const numberOfArrays = userArrayNumber;
-let arrays = [];
+// per 0 o per N volte quindi finche i < n continuo
 
 let i = 0;
-while (i <= numberOfArrays) {
-    const randomArrays = Math.floor(Math.random() * 100) + 10;
-    arrays.push(randomArrays);
-    i++
-    console.log(arrays);
-}
+
+// finche
+
+while (i < n) {
+
+    // creo un array
+
+    const arrayGenerator = [];
+
+    // creo elementi dentro array da 0 a 10
+
+    let y = 0;
+    while (y < 10) {
+
+        // genero un numero casuale da 0 a 100
+
+        let randoNumber = Math.floor(Math.random() * 100) + 1;
+
+        arrayGenerator.push(randoNumber);
+
+        y++;
+    }
+
+    // stampo l'array
+    i++;
 
 
+};
